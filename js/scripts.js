d4a850081daee22b5d130fd6bcde31af033497c8
Adam Craig and Kellie Olson <kellie@>
2016-03-24 11:10:47 -0700

    add branching and form functionality

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("form#dating").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age <= 30 && gender === 'female') {
+      $("#celeb1").show();
+      $("#celeb2").hide();
+      $("#celeb3").hide();
+      $("#celeb4").hide();
+    } else if (age > 30 && gender === 'female') {
+      $("#celeb2").show();
+      $("#celeb1").hide();
+      $("#celeb3").hide();
+      $("#celeb4").hide();
+    } else if (age <= 30 && gender === 'male') {
+      $("#celeb3").show();
+      $("#celeb1").hide();
+      $("#celeb2").hide();
+      $("#celeb4").hide();
+    } else {
+      $("#celeb4").show();
+      $("#celeb1").hide();
+      $("#celeb2").hide();
+      $("#celeb3").hide();
+    }
+    event.preventDefault();
+  });
+});
